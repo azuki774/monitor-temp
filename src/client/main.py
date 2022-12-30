434327c06596f4099d237eddf6a3f1192f8f5c45
azuki774 <azuki774s@gmail.com>
2022-12-31 01:55:01 +0900

    add logging

--- src/client/main.py
+++ src/client/main.py
@@ -4,6 +4,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 import remo
 import openweather
 import sys
+import logging
 
 REMOTE_HOST = os.environ['INFLUXDB_HOST']
 ORG = os.environ['INFLUXDB_ORG']
@@ -13,7 +14,7 @@ TOKEN = os.environ['INFLUXDB_TOKEN']
 def main():
     args = sys.argv
     if len(args) != 2:
-        print("invalid args")
+        logging.error("invalid args")
         sys.exit(1)
     
     target = args[1]
@@ -21,17 +22,20 @@ def main():
     write_api = client.write_api(write_options=SYNCHRONOUS)
     pointList = []
 
-    print(target)
     if target == 'remo':
+        logging.info("fetch remo API")
         pointList = remo.remo_main()
     elif target == 'open':
+        logging.info("fetch open weather API")
         pointList = openweather.open_main()
     else:
-        print("unknown args")
+        logging.error("unknown args")
         sys.exit(1)
 
     for p in pointList:
         write_api.write(bucket=BUCKET, record=p)
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s:%(name)s - %(message)s")
     main()
