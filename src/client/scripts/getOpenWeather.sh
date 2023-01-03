9292ef1bb6961239c74f988716f6d0ea6a62d743
azuki774 <azuki774s@gmail.com>
2023-01-03 15:13:08 +0900

    hotfix scripts open

--- src/client/scripts/getOpenWeather.sh
+++ src/client/scripts/getOpenWeather.sh
@@ -2,4 +2,4 @@
 LAT=$1
 LON=$2
 APPID=$3
-curl "https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&units=metric&lang=ja&appid=${APPID}"
+curl "https://api.openweathermap.org/data/2.5/onecall?lat=${LAT}&lon=${LON}&units=metric&lang=ja&appid=${APPID}" | jq '.current'
